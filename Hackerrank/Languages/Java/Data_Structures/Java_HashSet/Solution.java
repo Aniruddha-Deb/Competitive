b52895cecf6e52971752e9135b50bab91d49cc76
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2017-07-04 15:54:06 +0530

    Solved 2 problems

---
+++ Hackerrank/Languages/Java/Data_Structures/Java_HashSet/Solution.java
@@ -0,0 +1,6 @@
+        Set<String> leftSet = new HashSet<>();
+
+        for( int i=0; i<t; i++ ) {
+            leftSet.add( pair_left[i] + " " + pair_right[i] );
+            System.out.println( leftSet.size() );
+        }
