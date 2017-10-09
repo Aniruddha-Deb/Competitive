55d7cb0bde7d8d3754c204d0f5deb7778221923d
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-10-09 17:34:39 +0530

    Solved 2 problems

---
+++ Hackerrank/Languages/Java/Advanced/Java_MD5/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.security.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner( System.in );
+        MessageDigest m = MessageDigest.getInstance( "MD5" );
+        m.reset();
+        m.update( sc.nextLine().getBytes() );
+        for( byte i : m.digest() ) {
+            System.out.print( String.format( "%02x", i ) );
+        }
+        System.out.println();    
+    }
+}
