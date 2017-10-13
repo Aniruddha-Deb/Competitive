2a0e804cc17f33befb6e9391d4cce385824346d7
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-10-13 15:19:40 +0530

    Solved a bit manipulation problem

---
+++ Hackerrank/Algorithms/Bit_Manipulation/Lonely_Integer/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = in.nextInt();
+                
+        for(int a_i = 0; a_i<n-1; a_i++){
+            a ^= in.nextInt();
+        }
+        System.out.println(a);
+    }
+}
+
