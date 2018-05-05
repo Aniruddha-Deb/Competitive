273d8ac0d6bb0b6553ccba9e39fb0c6d91bb0a58
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2018-05-05 16:52:49 +0530

    Got started with UNIX shell

---
+++ Hackerrank/Languages/Unix_Shell/Getting_Started_With_Conditionals/Solution.java
@@ -0,0 +1,5 @@
+read y
+if [ $y = "Y" -o $y = "y" ]
+then echo "YES"
+else echo "NO"
+fi
---
+++ Hackerrank/Languages/Unix_Shell/The_World_of_Numbers/Solution.java
@@ -0,0 +1,8 @@
+
+
+read int1
+read int2
+echo $(($int1+$int2))
+echo $(($int1-$int2))
+echo $(($int1*$int2))
+echo $(($int1/$int2))
